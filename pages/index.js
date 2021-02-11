9e3848693e1cbeea45cb592bed03f56441319f49
kisihara_c <kisihara.c@gmail.com>
2021-02-11 22:01:26 +0900

    makingBone1

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
+        <title>pomodoro.c</title>
+      </Head>
+
+    
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          pomodoro.c
+        </h1>
+        <p>
+          {counter()}
+        </p>
+      </main>
+    </div>
+
+    /*
+    <div className={styles.container}>
+      <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -24,29 +41,6 @@ export default function Home() {
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -61,5 +55,11 @@ export default function Home() {
         </a>
       </footer>
     </div>
+
+    */
   )
+}
+
+let counter = (props) => {
+  return "1";
 }
